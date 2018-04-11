9205aa5075f2f6923be1ecd08320292e56391e3d
josephricklefs <josephricklefs@yahoo.com>
2018-04-10 20:17:21 -0500

    more of the same

--- App11/App11/MainPage.xaml.cs
+++ App11/App11/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -12,6 +13,24 @@ namespace App11
 		public MainPage()
 		{
 			InitializeComponent();
+		    DoStchuff();
 		}
+
+	    public async void DoStchuff()
+	    {
+	        HttpClient client = new HttpClient();
+
+	        try
+	        {
+	            var details = await client.GetStringAsync("http://api.josephricklefs.com/api/currentraceinfo");
+
+	            System.Diagnostics.Debug.Write("11111111111111111" + details);
+	        }
+	        catch (Exception e)
+	        {
+	            var ooo = e;
+	            throw;
+	        }
+	    }
 	}
 }
